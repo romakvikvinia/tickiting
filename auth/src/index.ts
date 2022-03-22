1f7332d9a0d97fcd7b2eb0c817deba21fea65f0e
roma <romakvikvinia@gmail.com>
2022-03-22 22:39:25 +0400

    fix

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongooes from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("Starting up...");
+  console.log("Starting up....");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
